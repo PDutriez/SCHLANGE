3bdbe8532a8e1b17767cf409b7f9fbc8187f5194
pdutriez <pdutriez@itba.edu.ar>
2019-01-23 12:47:26 -0300

    funciona todo pero imprime cosas TURBIAS cuando esta vacio el txt

--- FRESH START/common_operations.c
+++ FRESH START/common_operations.c
@@ -3,14 +3,16 @@
 #include "common_operations.h"
 #define MAX_LINE 256
 #define LOOSER 4 //Valor que devuelve CHKSCORE si no fue mejor que nadie
+#define MAX_SCORES 3 //Cantidad de jugadores que quiero en la lista
 
-void main(void)
+void main(void)//MAIN DE PRUEBA
 {
-  char prueba=chkscore("score.txt",33);
+  char prueba=chkscore("score.txt",295);
   printf("POSICION:%d\n",prueba);
   if(prueba&&prueba!=LOOSER)
   {
-    writescore("score.txt","SABR",33);
+    writescore("score.txt","PEPE",295);
+    printf("NEW HIGH SCORE\n");
   }
   else
   {
@@ -33,7 +35,8 @@ char chkscore(char* topscores,int score)
       while (fgets(line,sizeof(line),archivo) != NULL)
       {//Primero buscamos el PUNTAJE
 //Empezamos en 1 para saltear la enumeracion y buscamos el SCORE del renglon
-            for(counter=1;(line[counter]<'0')||(line[counter]>'9');++counter);
+            for(counter=1;((line[counter]<'0')||(line[counter]>'9'))&&counter<MAX_LINE;++counter);
+            printf("%s\n",line);
             if(score<=conver2int(&line[counter]))
             {
               ++position;//Implica que es menor al puntaje actual
@@ -47,57 +50,55 @@ char chkscore(char* topscores,int score)
 int writescore(char* topscores,char* name,int score)
 {
   int position=chkscore(topscores,score);
-  FILE* archivo=fopen(topscores, "r");
-  if(archivo==NULL)
+  if(position&&position!=LOOSER)
   {
-      printf("\nNo se pudo abrir el archivo\n");
-      return 0;//Indicamos que no existe el archivo
-  }
-  else
-  {
-      FILE* copia=fopen("copia.txt","w");
-      if(copia==NULL)
+      FILE* archivo=fopen(topscores, "r");
+      if(archivo==NULL)
       {
-          printf("\nNo se pudo cambiar la tabla\n");
-          fclose(archivo);
-          return 0;
+          printf("\nNo se pudo abrir el archivo\n");
+          return 0;//Indicamos que no existe el archivo
       }
       else
       {
-          char line_counter=0,line[MAX_LINE],copyline[MAX_LINE];
-          while ((!feof(archivo))&&line_counter<LOOSER)
-          {//Verificamos que no se haya terminado el archivo
-            strcpy(line, "\0");//Limpio el "copia-lineas"
-            fgets(line, MAX_LINE, archivo);
-            if ((!feof(archivo))&&line_counter<LOOSER)//Si todavia no se termino...
-            {
-                line_counter++;//Aumentamos el contador de lineas
-                if (line_counter != position)
-                {
-                    fprintf(copia, "%s", line);
-                }
-                else
-                {
-                    strcpy(copyline,line);
-                    sprintf(line,"%d %s %d\n",line_counter,name,score);
-                    fprintf(copia, "%s", line);
-                    ++copyline[0];
-                    fprintf(copia, "%s", copyline);
-                }
-                if(line_counter!=line[0])
-                {
-                    ++line[0];
-                    fprintf(copia, "%s", line);
-                }
+        FILE* copia=fopen("copia.txt","w");
+        if(copia==NULL)
+        {
+            printf("\nNo se pudo cambiar la tabla\n");
+            fclose(archivo);
+            return 0;
+        }
+        else
+        {
+            int line_counter;
+            char lines[MAX_SCORES+1][MAX_LINE];
+            for(line_counter=0;line_counter<MAX_SCORES;++line_counter)
+                  strcpy(lines[line_counter],"\0");//Limpiamos TODO
+
+            for(line_counter=0;(!feof(archivo))&&line_counter<MAX_SCORES;++line_counter)
+            {//Verificamos que no se haya terminado el archivo
+              fgets(lines[line_counter], MAX_LINE, archivo);//Copiamos el renglon
             }
-          }
-          fclose(archivo);//No te olvides de cerrrar lo que abris
-          fclose(copia);
-          remove(topscores);// remove the original file
-          rename("copia.txt", topscores);// rename the temporary file to original name
-          return 1;
-      }
+
+            for( ;position<=line_counter;--line_counter)
+            {//Corremos todos uno hacia abajo
+              strcpy(lines[line_counter],lines[line_counter-1]);
+              ++lines[line_counter][0];//Aumentamos el indice
+            }
+
+            sprintf(lines[position-1],"%d %s %d\n",position,name,score);//Copiamos el nuevo PUNTAJE
+
+            for(line_counter=0;(line_counter<MAX_SCORES)&&lines[line_counter][0]!='\0';++line_counter)
+                fprintf(copia, "%s", lines[line_counter]);//Copiamos el archivo
+
+            fclose(archivo);//No te olvides de cerrrar lo que abris
+            fclose(copia);
+            remove(topscores);// remove the original file
+            rename("copia.txt", topscores);// rename the temporary file to original name
+            return 1;
+        }
+    }
   }
+  return 0; //No se puede cambiar el archivo
 }
 
 int conver2int(char* string)
